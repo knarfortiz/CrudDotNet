272afa81c5b081d8400833ae319c968a08fc9633
knarfortiz <o.g.knarf@hotmail.com>
2021-03-20 22:33:32 -0500

    Elaboracion de la apiCRUD

---
+++ BackBasicCRUD/BackBasicCRUD/Controllers/CervezaController.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// App
+using BackBasicCRUD.Models.Response;
+using BackBasicCRUD.Models;
+using BackBasicCRUD.Models.Request;
+
+namespace BackBasicCRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CervezaController : ControllerBase
+    {
+        // GET: api/<CervezaController>
+        [HttpGet]
+        public IActionResult ListarCervezas()
+        {
+            Respuesta oRta = new();
+            try
+            {
+                using (BasicCrudContext db = new())
+                {
+                    var lst = db.Cervezas.ToList();
+                    oRta.Exito = 1;
+                    oRta.Data = lst;
+                }
+            }
+            catch (Exception ex)
+            {
+                oRta.Mensaje = ex.Message;
+            }
+            return Ok(oRta);
+        }
+
+        // GET: api/<CervezaController>/2
+        [HttpGet("{id}")]
+        public IActionResult ListarCerveza(int id)
+        {
+            Respuesta oRta = new();
+            try
+            {
+                using (BasicCrudContext db = new())
+                {
+                    Cerveza oCerveza = db.Cervezas.Find(id);
+                    if (oCerveza == null)
+                    {
+                        oRta.Exito = 0;
+                        oRta.Mensaje = "No existe el elemento";
+                    }
+                    else
+                    {
+                        oRta.Exito = 1;
+                        oRta.Data = oCerveza;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                oRta.Mensaje = ex.Message;
+            }
+            return Ok(oRta);
+        }
+
+        // POST: api/<CervezaController>
+        [HttpPost]
+        public IActionResult CrearCerveza(CervezaRequest model)
+        {
+            Respuesta oRta = new();
+            try
+            {
+                using (BasicCrudContext db = new())
+                {
+                    Cerveza oCerveza = new();
+                    oCerveza.Nombre = model.Nombre;
+                    oCerveza.Marca = model.Marca;
+                    db.Cervezas.Add(oCerveza);
+                    db.SaveChanges();
+                    oRta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                oRta.Mensaje = ex.Message;
+            }
+            return Ok(oRta);
+        }
+
+        // PUT: api/<CervezaController>
+        [HttpPut]
+        public IActionResult EditarCerveza(CervezaRequest model)
+        {
+            Respuesta oRta = new();
+            try
+            {
+                using (BasicCrudContext db = new())
+                {
+                    Cerveza oCerveza = db.Cervezas.Find(model.Id);
+                    oCerveza.Nombre = model.Nombre;
+                    oCerveza.Marca = model.Marca;
+                    db.Entry(oCerveza).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    db.SaveChanges();
+                    oRta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                oRta.Mensaje = ex.Message;
+            }
+            return Ok(oRta);
+        }
+
+        // DELETE: api/<CervezaController>/2
+        [HttpDelete("{id}")]
+        public IActionResult EliminarCerveza(int id)
+        {
+            Respuesta oRta = new();
+            try
+            {
+                using (BasicCrudContext db = new BasicCrudContext())
+                {
+                    Cerveza oCerveza = db.Cervezas.Find(id);
+                    db.Remove(oCerveza);
+                    db.SaveChanges();
+                    oRta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                oRta.Mensaje = ex.Message;
+            }
+            return Ok(oRta);
+        }
+    }
+}
---
+++ BackBasicCRUD/BackBasicCRUD/Models/BasicCrudContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace BackBasicCRUD.Models
+{
+    public partial class BasicCrudContext : DbContext
+    {
+        public BasicCrudContext()
+        {
+        }
+
+        public BasicCrudContext(DbContextOptions<BasicCrudContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cerveza> Cervezas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;  Database=BasicCrud; Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Modern_Spanish_CI_AS");
+
+            modelBuilder.Entity<Cerveza>(entity =>
+            {
+                entity.ToTable("Cerveza");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Marca)
+                    .IsRequired()
+                    .HasMaxLength(10)
+                    .HasColumnName("marca")
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(10)
+                    .HasColumnName("nombre")
+                    .IsFixedLength(true);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
